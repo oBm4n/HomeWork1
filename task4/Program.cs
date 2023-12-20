e08b3c0c058455bc455424254b56dd5bfe383e79
Beydin <spbservers@list.ru>
2023-12-20 22:44:44 +0300

    Научился определять больший из пяти

---
+++ task4/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 6;
+int b = 1;
+int c = 4;
+int d = 7;
+int e = 3;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
+
