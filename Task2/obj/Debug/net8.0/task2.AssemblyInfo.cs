0364cba917ed7e5ec8482eed046af9c355512386
Beydin <spbservers@list.ru>
2023-12-20 22:26:03 +0300

    Добавили деление

--- Task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+039672527c1927fa14f2dcfda1f8eb3243868a0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
