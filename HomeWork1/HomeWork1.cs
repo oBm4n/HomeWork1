dc39aea72c520aab106dbc2f7df76f63f1720c9c
Beydin <spbservers@list.ru>
2024-01-15 23:05:45 +0300

    Sdelal dz

---
+++ HomeWork1/HomeWork1.cs
@@ -0,0 +1,13 @@
+﻿void Delenie(int num)
+{
+    if(num % 7 == 0 && num % 23 == 0)
+    {
+        System.Console.WriteLine("OK");
+    } else
+    {
+        System.Console.WriteLine("NO");
+    }
+}
+System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Delenie(number);
